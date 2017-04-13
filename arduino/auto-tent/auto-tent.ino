fafa76fa883162ae956f07002f093ae2e65580d4
Jason Poindexter <poindexter.json@gmail.com>
2017-04-13 15:20:18 -0700

    Update auto-tent.ino with LED functions
    Add LED functions to fade in and out lights.  Not tested.

--- arduino/auto-tent/auto-tent.ino
+++ arduino/auto-tent/auto-tent.ino
@@ -1,11 +1,22 @@
 // auto-tent
 
+//FastLED Setup
+#include <FastLED.h>
+#define LED_PIN     5
+#define NUM_LEDS    50
+#define MAX_BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+int color = CRGB::White;
+
 // HC-SR501 Motion Detector setup
 int ledPin = 13;  // LED on Pin 13 of Arduino
 int pirPin = 8; // Input for HC-S501
 int motionDetected; // Place to store read PIR Valu
 int detectionBlock = 3000; // motion sensor is not able to detect for 3 seconds
-int noMotionPeriod = 5000 + detectionBlock; // how many second to wait while no motion is detected befor fading out lights
+int noMotionPeriod = 10000 + detectionBlock; // how many second to wait while no motion is detected befor fading out lights
 long previousMillis = 0;        // will store last time LED was updated
 bool timeOutReached = true;
 
@@ -13,9 +24,14 @@ bool currentMotionState = LOW;
 bool previousMotionState = LOW;
 
 void setup() {
+  delay( 3000 ); // power-up safety delay
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  MAX_BRIGHTNESS );
+  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  
+
+  //initialize motion sensor
   pinMode(ledPin, OUTPUT);
   pinMode(pirPin, INPUT);
   digitalWrite(ledPin, LOW);
@@ -34,10 +50,12 @@ void loop() {
     if(currentMotionState == true){
       previousMotionState = currentMotionState;
       Serial.println("Motion is detected");
-      turnOnLeds();
+      if(timeOutReached){
+        turnOnLeds();
+      }
     }
     else{
-      Serial.println(currentMillis - previousMillis);
+      //Serial.println(currentMillis - previousMillis);
       if(currentMotionState != previousMotionState){
         //Serial.println("Hit motion detection switch");
         previousMillis = currentMillis;
@@ -47,7 +65,7 @@ void loop() {
       }
       if( currentMillis - previousMillis > noMotionPeriod ){
         timeOutReached = true;
-        //Serial.println("Motion is NOT detected");
+        Serial.println("Motion is NOT detected");
         previousMillis = currentMillis;
         turnOffLeds();
       }
@@ -55,12 +73,26 @@ void loop() {
   }
 }
 
+
 void turnOnLeds(){
   Serial.println("Turn LEDs ON");
+  int fadeSpeed = 1; //how fast to fade from Black to MAX_BRIGHTNESS
+  for(int brightness = 1; brightness <= MAX_BRIGHTNESS; brightness++){
+    for( int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = color; 
+    }
+    FastLED.setBrightness( brightness );
+    Serial.println(brightness);
+    FastLED.show();
+    delay( (fadeSpeed * 1000) / MAX_BRIGHTNESS );
+  }
 }
 
 void turnOffLeds(){
   Serial.println("Turn LEDs OFF");
+  for( int i = 0; i < NUM_LEDS; i++) {
+    // Dim a color by 25% (64/256ths)
+    // eventually fading to full black
+    leds[i].fadeToBlackBy( 64 );
+  }
 }
-
-
